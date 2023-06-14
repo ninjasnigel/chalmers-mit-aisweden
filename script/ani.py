22bd27133041a2f3a7a6c39578f684714ff7ea8e
David Laessker <davidlaessker@gmail.com>
2023-06-14 15:13:24 +0200

    q

--- script/ani.py
+++ script/ani.py
@@ -3,63 +3,55 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# Physical constants
-g = 9.8
-m1 = 1.0
-m2 = 1.0
-l1 = 1.0
-l2 = 1.0
+# Define the system parameters
+m1, m2, L1, L2, g = 1.0, 1.0, 1.0, 1.0, 9.81
 
-# The system of equations describing the double pendulum
-def equations(y0, t):
-    theta1, z1, theta2, z2 = y0
+def equations(Y, t, m1, m2, L1, L2, g):
+    """The function that describes the ODE system."""
+    theta1, z1, theta2, z2 = Y
     c, s = np.cos(theta1-theta2), np.sin(theta1-theta2)
     theta1_dot = z1
-    z1_dot = (m2*g*np.sin(theta2) - m2*s*(l1*z1**2*c + l2*z2**2) - (m1+m2)*g*np.sin(theta1)) / l1 / (m1 + m2*s**2)
+    z1_dot = (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) - (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
     theta2_dot = z2
-    z2_dot = ((m1+m2)*(l1*z1**2*s - g*np.sin(theta2) + g*np.sin(theta1)*c) + m2*l2*z2**2*s*c) / l2 / (m1 + m2*s**2)
+    z2_dot = ((m1+m2)*(L1*z1**2*s - g*np.sin(theta2) + g*np.sin(theta1)*c) + m2*L2*z2**2*s*c) / L2 / (m1 + m2*s**2)
     return theta1_dot, z1_dot, theta2_dot, z2_dot
 
-# initial conditions: angles are in radians and angular velocities are in rad/s
-y0 = [3*np.pi/7, 0, 3*np.pi/4, 0]
+# Set initial conditions
+Y0 = [np.pi/2, 0.0, np.pi/2, 0.0]
 
-# time array for the solution
-t = np.linspace(0, 10, 250)
+# Create an array of time values to solve at
+t = np.linspace(0, 20, 1000)
 
-# solve the ODE using scipy.integrate.
-y = odeint(equations, y0, t)
+# Solve the ODE system
+Y = odeint(equations, Y0, t, args=(m1, m2, L1, L2, g))
 
-# Transform to Cartesian coordinates
-x1 = l1 * np.sin(y[:, 0])
-y1 = -l1 * np.cos(y[:, 0])
-x2 = x1 + l2 * np.sin(y[:, 2])
-y2 = y1 - l2 * np.cos(y[:, 2])
+# Create a figure and axis to plot on
+fig, ax = plt.subplots()
 
-fig, ax = plt.subplots(figsize=(6,6))
+# Create two line objects for the rods of the pendulum
+line, = ax.plot([], [], 'o-', lw=2)
+trace, = ax.plot([], [], 'r-', lw=1)
 
-ax = plt.gca()
-ax.set_aspect('equal', adjustable='datalim')
-
-# Set larger limits
+# Define the x and y limits of the plot
 ax.set_xlim(-2, 2)
 ax.set_ylim(-2, 2)
 
-pendulum_line, = ax.plot([], [], 'o-', lw=2)
-path_line, = ax.plot([], [], 'r-', lw=0.5)  # New line for the path
-
+# This function initializes the animation
 def init():
-    pendulum_line.set_data([], [])
-    path_line.set_data([], [])  # Initialize path line data
-    return pendulum_line, path_line,
+    line.set_data([], [])
+    trace.set_data([], [])
+    return line, trace
 
+# This function is called for each frame of the animation
 def animate(i):
-    thisx = [0, x1[i], x2[i]]
-    thisy = [0, y1[i], y2[i]]
+    thisx = [0, L1*np.sin(Y[i, 0]), L1*np.sin(Y[i, 0]) + L2*np.sin(Y[i, 2])]
+    thisy = [0, -L1*np.cos(Y[i, 0]), -L1*np.cos(Y[i, 0]) - L2*np.cos(Y[i, 2])]
+    line.set_data(thisx, thisy)
+    trace.set_data(thisx[2:], thisy[2:])
+    return line, trace
 
-    pendulum_line.set_data(thisx, thisy)
-    path_line.set_data(x2[:i], y2[:i])  # Set data for path line
-    return pendulum_line, path_line,
+# Create the animation
+ani = animation.FuncAnimation(fig, animate, np.arange(1, len(Y)), interval=25, blit=True, init_func=init)
 
-ani = animation.FuncAnimation(fig, animate, frames=len(t), init_func=init, blit=True)
-
+# Show the animation
 plt.show()
